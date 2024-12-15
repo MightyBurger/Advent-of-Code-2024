91c4af47e6e85f782dab8877f50c78b3ec08d596
Jordan Johnson <52584279+MightyBurger@users.noreply.github.com>
2024-12-14 21:03:57 -0700

    struggling with doing this on linux

--- day13/src/bin/part1.rs
+++ day13/src/bin/part1.rs
@@ -1,9 +1,24 @@
+#![allow(unused_variables)]
+
+use regex::Regex;
+
 fn process(input: &str) -> i32 {
+    let rx = Regex::new(
+        r#"Button A: X\+(?<AX>\d+), Y\+(?<AY>\d+)\s+Button B: X\+(?<BX>\d+), Y\+(?<BY>\d+)\s+Prize: X=(?<PX>\d+), Y=(?<PY>\d+)"#,
+    ).unwrap();
+
+    for capture in rx.captures_iter(input) {
+        let x = capture;
+    }
+
+    for i in 0..5 {
+        println!("what is this");
+    }
     todo!()
 }
 
 fn main() {
-    let input = include_str!("../../../dayxx/input1.txt");
+    let input = include_str!("../../../day13/input1.txt");
     let result = process(input);
     println!("The result is {}", result);
 }
@@ -18,7 +33,7 @@ mod tests {
 
     #[test]
     fn test() {
-        let check = include_str!("../../../dayxx/check1.txt");
+        let check = include_str!("../../../day13/check1.txt");
         assert_eq!(process(check), 11)
     }
 }
