0f0b7b3be8c647bcd612bf9f0b09dfa0699a8f24
Jordan Johnson <52584279+MightyBurger@users.noreply.github.com>
2024-12-15 16:13:54 -0700

    very simple part 1

--- day13/src/bin/part1.rs
+++ day13/src/bin/part1.rs
@@ -1,4 +1,53 @@
 #![allow(unused_variables)]
+#![allow(dead_code)]
+#[derive(Default, Debug, Clone, Copy, PartialEq, Eq, PartialOrd, Ord, Hash)]
+struct Pos {
+    col: i32,
+    row: i32,
+}
+
+impl Pos {
+    // col then row
+    fn new(col: i32, row: i32) -> Self {
+        Self { col, row }
+    }
+    // x then y
+    fn xy(x: i32, y: i32) -> Self {
+        Self { col: y, row: x }
+    }
+}
+
+use std::ops::{Add, Mul, Sub};
+impl Add for Pos {
+    type Output = Self;
+    fn add(self, other: Self) -> Self::Output {
+        Self {
+            col: self.col + other.col,
+            row: self.row + other.row,
+        }
+    }
+}
+
+impl Sub for Pos {
+    type Output = Self;
+    fn sub(self, other: Self) -> Self::Output {
+        Self {
+            col: self.col - other.col,
+            row: self.row - other.row,
+        }
+    }
+}
+
+impl Mul<i32> for Pos {
+    type Output = Self;
+
+    fn mul(self, rhs: i32) -> Self::Output {
+        Self {
+            col: self.col * rhs,
+            row: self.row * rhs,
+        }
+    }
+}
 
 use regex::Regex;
 
@@ -7,14 +56,32 @@ fn process(input: &str) -> i32 {
         r#"Button A: X\+(?<AX>\d+), Y\+(?<AY>\d+)\s+Button B: X\+(?<BX>\d+), Y\+(?<BY>\d+)\s+Prize: X=(?<PX>\d+), Y=(?<PY>\d+)"#,
     ).unwrap();
 
-    for capture in rx.captures_iter(input) {
-        let x = capture;
-    }
+    let mut sum: i32 = 0;
+    for group in input.split("\n\n") {
+        let Some(cap) = rx.captures(group) else {
+            continue;
+        };
+        let btn_a = Pos::xy(cap["AX"].parse().unwrap(), cap["AY"].parse().unwrap());
+        let btn_b = Pos::xy(cap["BX"].parse().unwrap(), cap["BY"].parse().unwrap());
+        let target = Pos::xy(cap["PX"].parse().unwrap(), cap["PY"].parse().unwrap());
 
-    for i in 0..5 {
-        println!("what is this");
+        let mut min: Option<i32> = None;
+        for a in 0..=100 {
+            for b in 0..=100 {
+                if btn_a * a + btn_b * b == target {
+                    match min {
+                        Some(tmin) if 3 * a + 1 * b > tmin => min = Some(3 * a + 1 * b),
+                        Some(_) => (),
+                        None => min = Some(3 * a + 1 * b),
+                    }
+                }
+            }
+        }
+        if let Some(min) = min {
+            sum += min;
+        }
     }
-    todo!()
+    sum
 }
 
 fn main() {
@@ -34,6 +101,6 @@ mod tests {
     #[test]
     fn test() {
         let check = include_str!("../../../day13/check1.txt");
-        assert_eq!(process(check), 11)
+        assert_eq!(process(check), 480)
     }
 }
