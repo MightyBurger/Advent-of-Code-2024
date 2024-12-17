cf2bd866b04811c595d637f2799815ca30376796
Jordan Johnson <52584279+MightyBurger@users.noreply.github.com>
2024-12-16 22:22:30 -0700

    the tree is sidways lol

---
+++ day14/src/bin/part1.rs
@@ -0,0 +1,126 @@
+#![allow(unused_variables)]
+#![allow(dead_code)]
+
+#[derive(Default, Debug, Clone, Copy, PartialEq, Eq, PartialOrd, Ord, Hash)]
+struct Vec2 {
+    col: i32,
+    row: i32,
+}
+
+impl Vec2 {
+    // col then row
+    fn new(col: i32, row: i32) -> Self {
+        Self { col, row }
+    }
+    // x then y
+    fn xy(x: i32, y: i32) -> Self {
+        Self { col: y, row: x }
+    }
+}
+
+use std::ops::{Add, Mul, Sub};
+impl Add for Vec2 {
+    type Output = Self;
+    fn add(self, other: Self) -> Self::Output {
+        Self {
+            col: self.col + other.col,
+            row: self.row + other.row,
+        }
+    }
+}
+
+impl Sub for Vec2 {
+    type Output = Self;
+    fn sub(self, other: Self) -> Self::Output {
+        Self {
+            col: self.col - other.col,
+            row: self.row - other.row,
+        }
+    }
+}
+
+impl Mul<i32> for Vec2 {
+    type Output = Self;
+
+    fn mul(self, rhs: i32) -> Self::Output {
+        Self {
+            col: self.col * rhs,
+            row: self.row * rhs,
+        }
+    }
+}
+
+#[derive(Debug, Clone, PartialEq, Eq, PartialOrd, Ord)]
+struct Guard {
+    pos: Vec2,
+    vel: Vec2,
+}
+
+use regex::Regex;
+
+fn process(input: &str, cols: i32, rows: i32) -> i32 {
+    let rx = Regex::new(r#"p=(?<PX>\d+),(?<PY>\d+) v=(?<VX>[-\d]+),(?<VY>[-\d]+)"#).unwrap();
+
+    let mut guards: Vec<Guard> = input
+        .lines()
+        .filter_map(|line| {
+            let cap = rx.captures(line)?;
+            let pos = Vec2::xy(cap["PX"].parse().unwrap(), cap["PY"].parse().unwrap());
+            let vel = Vec2::xy(cap["VX"].parse().unwrap(), cap["VY"].parse().unwrap());
+            Some(Guard { pos, vel })
+        })
+        .collect();
+
+    let steps = 100;
+    for _ in 0..steps {
+        for guard in guards.iter_mut() {
+            guard.pos = guard.pos + guard.vel;
+            guard.pos.col = guard.pos.col.rem_euclid(cols);
+            guard.pos.row = guard.pos.row.rem_euclid(rows);
+        }
+    }
+
+    let midrow = (rows - 1) / 2;
+    let midcol = (cols - 1) / 2;
+
+    let mut safety1 = 0;
+    let mut safety2 = 0;
+    let mut safety3 = 0;
+    let mut safety4 = 0;
+
+    for guard in guards.iter() {
+        if (0..midcol).contains(&guard.pos.col) && (0..midrow).contains(&guard.pos.row) {
+            safety1 += 1;
+        } else if (0..midcol).contains(&guard.pos.col) && (midrow + 1..).contains(&guard.pos.row) {
+            safety2 += 1;
+        } else if (midcol + 1..).contains(&guard.pos.col) && (0..midrow).contains(&guard.pos.row) {
+            safety3 += 1;
+        } else if (midcol + 1..).contains(&guard.pos.col) && (midrow + 1..).contains(&guard.pos.row)
+        {
+            safety4 += 1;
+        }
+    }
+
+    safety1 * safety2 * safety3 * safety4
+}
+
+fn main() {
+    let input = include_str!("../../../day14/input1.txt");
+    let result = process(input, 103, 101);
+    println!("The result is {}", result);
+}
+
+// ----------------------------------------------------
+// -------------------- Unit Tests --------------------
+// ----------------------------------------------------
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn test() {
+        let check = include_str!("../../../day14/check1.txt");
+        assert_eq!(process(check, 11, 7), 12)
+    }
+}
